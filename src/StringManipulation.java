2fe1185fc52d79a7ac3e9535063e26bad7442720
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2026-05-09 07:35:36 +0530

    debugged issue and brute force implimentation

--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -5,9 +5,11 @@ public class StringManipulation {
 	public static void main(String args[]) {
 		String str = "hello";
 		String rev = "";
-		for(int i = str.length()-1; i > 0; i-- ) {
+		for(int i = str.length()-1; i >= 0; i-- ) {
 			rev += str.charAt(i);
 		}
+		System.out.println("Original String: " + str);
+		System.out.println("Reversed String: " + rev);
 	}
 
 }
