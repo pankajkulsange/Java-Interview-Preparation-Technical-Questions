05997ed7d49e457d1e6d0ab82b3d2694307e7ebb
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2026-05-09 23:34:10 +0530

    palindrom questaion and answer added in python

--- python/Basics.py
+++ python/Basics.py
@@ -8,3 +8,19 @@ print(rev)
 
 reversed_str = str[::-1]
 print(reversed_str)
+
+# string is  palindrome or not
+# str1 = "madam" # palindrome string
+str1 = "Pankaj" #non palindrome string
+rev_str1 = ""
+for i in range(len(str1)-1, -1, -1):
+    rev_str1 += str1[i]
+if str1 == rev_str1:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+# build in method
+if str1 == str[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
