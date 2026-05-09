a9243b03d51f3643ae425ca39b83c1375ae4ce81
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2026-05-09 21:08:26 +0530

    added build in methods for javascript and java

--- JavaScript/Basics.js
+++ JavaScript/Basics.js
@@ -6,3 +6,8 @@ for (let i = str.length - 1; i >= 0; i--) {
 }
 console.log("Original String: " + str);
 console.log("Reversed String: " + rev);
+
+// using build in array methods
+let reversed = str.split("").reverse().join("");
+console.log("Original String: " + str);
+console.log("Reversed String: " + reversed);
--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -3,6 +3,11 @@ public class StringManipulation {
 	
 	// reverse string
 	public static void main(String args[]) {
+		reverseString();
+		reverseStringUsingBuildInMethods();
+
+	}
+	public static void reverseString(){
 		String str = "hello";
 		String rev = "";
 		for(int i = str.length()-1; i >= 0; i-- ) {
@@ -10,6 +15,11 @@ public class StringManipulation {
 		}
 		System.out.println("Original String: " + str);
 		System.out.println("Reversed String: " + rev);
+	}
+	public static void reverseStringUsingBuildInMethods(){
+		String str = "hello";
+		String reversed = new StringBuilder(str).reverse().toString();
+		System.out.println(reversed);
 	}
 
 }
